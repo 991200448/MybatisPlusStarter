cfed29210e8707f6796adcc67640358c5251286e
991200448 <51320345+991200448@users.noreply.github.com>
2025-03-15 01:05:25 +0800

    Delete MybatisPlusStarter directory

--- MybatisPlusStarter/src/main/java/com/daisyPig/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.daisyPig.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.daisyPig.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}

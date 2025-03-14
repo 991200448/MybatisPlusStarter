e9bc46b569fc15bc53ebb521424ebd8a5baed7b8
991200448 <51320345+991200448@users.noreply.github.com>
2025-03-15 02:24:20 +0800

    Delete MybatisPlusStarter directory

--- MybatisPlusStarter/src/main/java/com/daisyPig/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.daisyPig.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.daisyPig.entity.User;
-
-public interface UserService extends IService<User> {
-}
